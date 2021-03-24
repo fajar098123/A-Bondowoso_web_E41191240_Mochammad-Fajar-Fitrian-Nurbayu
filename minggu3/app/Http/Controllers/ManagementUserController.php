a50609426f4dfb541fb230dae083d0f72a2a3f89
fajar098123 <fajar098123@gmail.com>
2021-03-25 01:29:40 +0700

    Minggu 3 / 2
    Membuat Model View Controller Pada Laravel (Membuat Controller dan Routing)

---
+++ minggu3/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return "Halo ini adalah method index, dalam controller ManagementUser.";
+        return "Method ini nantinya akan digunakan untuk mengambil semua data user";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return "Method ini nantinya akan digunakan untuk menciptakan data user yang baru";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "Method ini nantinya akan digunakan untuk mengambil satu data user dengan id=" . $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id=" . $id;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Method ini nantinya akan digunakan untuk mengubah data user dengan id=" . $id;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "Method ini nantinya akan digunakan untuk menghapus data user dengan id=" . $id;
+
+    }
+}
--- minggu3/routes/web.php
+++ minggu3/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ManagementUserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('user', ManagementUserController::class);
